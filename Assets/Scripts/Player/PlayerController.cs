08814ef46cb4a6f8e20f770fb780d4172aa511c0
george-593 <63100333+george-593@users.noreply.github.com>
2025-11-21 18:21:06 +0000

    feat: player movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    public Vector2 position = new Vector2();
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Movement
+        float hzInput = Input.GetAxis("Horizontal");
+        float vtInput = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(moveSpeed * hzInput, moveSpeed * vtInput);
+    }
+}
