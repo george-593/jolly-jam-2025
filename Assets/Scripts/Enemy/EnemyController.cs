f37742d4c8597113332e5a986c0858c2b99c49dc
george-593 <63100333+george-593@users.noreply.github.com>
2025-11-21 19:44:52 +0000

    feat: enemy moves towards player

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    public float speed = 3f;
+
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Move towards closest enemy
+        Vector2 myPos = transform.position;
+        Vector2 targetPos = target.position;
+        transform.position = Vector2.MoveTowards(myPos, targetPos, speed * Time.deltaTime);
+    }
+}
