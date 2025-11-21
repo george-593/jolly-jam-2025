20600b33bb2b9e7ef0c256172cc803b701e999ff
george-593 <63100333+george-593@users.noreply.github.com>
2025-11-21 20:00:16 +0000

    feat: Enemy collision with player damages player

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,12 +4,17 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
-
     public float speed = 3f;
+    public float damage = 5f;
+
+    private PlayerController player;
+    private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = target.gameObject.GetComponent<PlayerController>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -18,5 +23,15 @@ public class EnemyController : MonoBehaviour
         Vector2 myPos = transform.position;
         Vector2 targetPos = target.position;
         transform.position = Vector2.MoveTowards(myPos, targetPos, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.transform == target)
+        {
+            Debug.Log("[EnemyController.cs] collided with player");
+            // Deduct health from player
+            player.health -= damage;
+        }
     }
 }
