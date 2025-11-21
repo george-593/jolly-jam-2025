3826fa79aeefdc64d8eaca7ab61d2de96b1a2481
george-593 <63100333+george-593@users.noreply.github.com>
2025-11-21 23:13:25 +0000

    feat Enemy collision with player repeatedly deals damage

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -6,15 +6,17 @@ public class EnemyController : MonoBehaviour
     private Transform target;
     public float speed = 3f;
     public float damage = 5f;
+    // The time to wait before dealing damage.
+    public float damageInterval = 0.5f;
 
     private PlayerController player;
-    private Rigidbody2D rb;
+    private bool insidePlayer = false;
+    private float timeSinceLastDamage = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = target.gameObject.GetComponent<PlayerController>();
-        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -23,15 +25,37 @@ public class EnemyController : MonoBehaviour
         Vector2 myPos = transform.position;
         Vector2 targetPos = target.position;
         transform.position = Vector2.MoveTowards(myPos, targetPos, speed * Time.deltaTime);
+
+        // Deal damage to player after x amount of time
+        if (insidePlayer)
+        {
+            timeSinceLastDamage += Time.deltaTime;
+
+            if (timeSinceLastDamage >= damageInterval)
+            {
+                Debug.Log("[EnemyController.cs] dealing damage to player");
+                player.health -= damage;
+                timeSinceLastDamage = 0;
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.transform == target)
         {
-            Debug.Log("[EnemyController.cs] collided with player");
-            // Deduct health from player
-            player.health -= damage;
+            insidePlayer = true;
+            // Trigger a damage effect immediately
+            timeSinceLastDamage = damageInterval;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.transform == target)
+        {
+            insidePlayer = false;
+            timeSinceLastDamage = 0f;
         }
     }
 }
