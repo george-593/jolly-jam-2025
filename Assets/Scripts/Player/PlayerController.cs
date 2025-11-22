29945df6d5fe5694dee5bbd8a0a833c511fa2ff6
george-593 <63100333+george-593@users.noreply.github.com>
2025-11-22 12:04:17 +0000

    feat: make player rotation based on mouse pos

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,10 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float health = 100f;
+    public Camera camera;
+
     private Rigidbody2D rb;
+    private Vector2 mousePos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,5 +23,13 @@ public class PlayerController : MonoBehaviour
         float vtInput = Input.GetAxis("Vertical");
 
         rb.linearVelocity = new Vector2(moveSpeed * hzInput, moveSpeed * vtInput);
+
+        // Shooting Enemies
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+
+        // Make player follow mouse
+        Vector2 aimDir = mousePos - rb.position;
+        float aimAngle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 }
